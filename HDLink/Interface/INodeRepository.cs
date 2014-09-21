875da04543df7248def9391d3d1958d6ead8ff6c
PjMitchell <peter@hilltop-digital.co.uk>
2014-09-21 22:37:54 +0100

    Added INodeRepository<T>

--- HDLink/Interface/INodeRepository.cs
+++ HDLink/Interface/INodeRepository.cs
@@ -24,4 +24,26 @@ namespace HDLink
         /// <returns>Matching NIodes </returns>
         IEnumerable<INode> Get(IEnumerable<int> ids);
     }
+
+    /// <summary>
+    /// Gets Nodes from data source
+    /// </summary>
+    /// <typeparam name="T">Node type for Repository</typeparam>
+    public interface INodeRepository<T> : INodeRepository where T : INode 
+    {
+        /// <summary>
+        /// Gets INode by Id
+        /// </summary>
+        /// <param name="id">Node Id</param>
+        /// <returns>Matching INode</returns>
+        T Get(int id);
+
+        /// <summary>
+        /// Gets INodes by Ids
+        /// </summary>
+        /// <param name="ids">Node Ids</param>
+        /// <returns>Matching NIodes </returns>
+        IEnumerable<T> Get(IEnumerable<int> ids);
+    } 
+
 }
