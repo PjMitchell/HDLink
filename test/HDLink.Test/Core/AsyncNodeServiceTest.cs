4e41bf152eea0e769943b6d029324adbcc250bc4
PjMitchell <peter@hilltop-digital.co.uk>
2015-05-25 09:53:46 +0100

    Removed async tests to check they are not breaking runner

--- test/HDLink.Test/Core/AsyncNodeServiceTest.cs
+++ test/HDLink.Test/Core/AsyncNodeServiceTest.cs
@@ -32,70 +32,70 @@ namespace HDLink.Test.Core
         }
 
         #region Get(Node) Tests
-        [Fact]
-        public async Task Get_Node_ThrowsNullException_IfNodeNull()
-        {
-            await Assert.ThrowsAsync(typeof(ArgumentNullException), () => target.Get(null));          
-        }
+        //[Fact]
+        //public async Task Get_Node_ThrowsNullException_IfNodeNull()
+        //{
+        //    await Assert.ThrowsAsync(typeof(ArgumentNullException), () => target.Get(null));          
+        //}
 
-        [Fact]
-        public async Task Get_Node_ReturnsCorrectResult()
-        {
-            var result = await target.Get(StoryNode.RedRidingHood);
+        //[Fact]
+        //public async Task Get_Node_ReturnsCorrectResult()
+        //{
+        //    var result = await target.Get(StoryNode.RedRidingHood);
 
-            Assert.Equal(3, result.Count);
-            Assert.True(result.Contains(ActorNode.BigBadWolf));
-            Assert.True(result.Contains(ActorNode.GrannyRidingHood));
-            Assert.True(result.Contains(ActorNode.RedRidingHood));
-        }
+        //    Assert.Equal(3, result.Count);
+        //    Assert.True(result.Contains(ActorNode.BigBadWolf));
+        //    Assert.True(result.Contains(ActorNode.GrannyRidingHood));
+        //    Assert.True(result.Contains(ActorNode.RedRidingHood));
+        //}
         #endregion
 
         #region Get(Node, NodeType) Tests
-        [Fact]
-        public async Task Get_Node_NodeType_ThrowsNullException_IfNodeNull()
-        {
-            await Assert.ThrowsAsync(typeof(ArgumentNullException), () => target.Get(null, MockNodeTypes.Story));
-        }
+        //[Fact]
+        //public async Task Get_Node_NodeType_ThrowsNullException_IfNodeNull()
+        //{
+        //    await Assert.ThrowsAsync(typeof(ArgumentNullException), () => target.Get(null, MockNodeTypes.Story));
+        //}
 
-        [Fact]
-        public async Task Get_Node_NodeType_ThrowsNullException_IfNodeTypeNull()
-        {
-            await Assert.ThrowsAsync(typeof(ArgumentNullException), () => target.Get(ActorNode.BigBadWolf, (INodeType<StoryNode>)null));
-        }
+        //[Fact]
+        //public async Task Get_Node_NodeType_ThrowsNullException_IfNodeTypeNull()
+        //{
+        //    await Assert.ThrowsAsync(typeof(ArgumentNullException), () => target.Get(ActorNode.BigBadWolf, (INodeType<StoryNode>)null));
+        //}
 
-        [Fact]
-        public async Task Get_Node_NodeType_ReturnsCorrectResult()
-        {
-            var result = await target.Get(ActorNode.BigBadWolf, MockNodeTypes.Story);
+        //[Fact]
+        //public async Task Get_Node_NodeType_ReturnsCorrectResult()
+        //{
+        //    var result = await target.Get(ActorNode.BigBadWolf, MockNodeTypes.Story);
 
-            Assert.Equal(2, result.Count);
-            Assert.True(result.Contains(StoryNode.RedRidingHood));
-            Assert.True(result.Contains(StoryNode.ThreeLittlePigs));
-        }
+        //    Assert.Equal(2, result.Count);
+        //    Assert.True(result.Contains(StoryNode.RedRidingHood));
+        //    Assert.True(result.Contains(StoryNode.ThreeLittlePigs));
+        //}
         #endregion
 
         #region Get<T>(Node, NodeType) Tests
-        [Fact]
-        public async Task Get_T_Node_NodeType_ThrowsNullException_IfNodeNull()
-        {
-            await Assert.ThrowsAsync(typeof(ArgumentNullException), () => target.Get(null, MockNodeTypes.Story));
-        }
+        //[Fact]
+        //public async Task Get_T_Node_NodeType_ThrowsNullException_IfNodeNull()
+        //{
+        //    await Assert.ThrowsAsync(typeof(ArgumentNullException), () => target.Get(null, MockNodeTypes.Story));
+        //}
 
-        [Fact]
-        public async Task Get_T_Node_NodeType_ThrowsNullException_IfNodeTypeNull()
-        {
-            await Assert.ThrowsAsync(typeof(ArgumentNullException), () => target.Get<StoryNode>(ActorNode.BigBadWolf, null));
-        }
+        //[Fact]
+        //public async Task Get_T_Node_NodeType_ThrowsNullException_IfNodeTypeNull()
+        //{
+        //    await Assert.ThrowsAsync(typeof(ArgumentNullException), () => target.Get<StoryNode>(ActorNode.BigBadWolf, null));
+        //}
 
-        [Fact]
-        public async Task Get_T_Node_NodeType_ReturnsCorrectResult()
-        {
-            var result = await target.Get(ActorNode.BigBadWolf, MockNodeTypes.Story);
+        //[Fact]
+        //public async Task Get_T_Node_NodeType_ReturnsCorrectResult()
+        //{
+        //    var result = await target.Get(ActorNode.BigBadWolf, MockNodeTypes.Story);
 
-            Assert.Equal(2, result.Count);
-            Assert.True(result.Contains(StoryNode.RedRidingHood));
-            Assert.True(result.Contains(StoryNode.ThreeLittlePigs));
-        }
+        //    Assert.Equal(2, result.Count);
+        //    Assert.True(result.Contains(StoryNode.RedRidingHood));
+        //    Assert.True(result.Contains(StoryNode.ThreeLittlePigs));
+        //}
 
         #endregion
     }
