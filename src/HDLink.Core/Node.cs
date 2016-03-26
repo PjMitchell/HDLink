b4fed7f0b7c7dcea550c7b5d1d6f7cbb851aaadd
PjMitchell <peterjames.m@gmail.com>
2016-03-26 22:28:50 +0000

    Initial setup

---
+++ src/HDLink.Core/Node.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections.Generic;
+
+namespace HDLink.Core
+{
+    public class Node
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Type { get; set; }
+        public string Description { get; set; }
+        public List<NodeAttribute> Attributes { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            return Id.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+
+    public class AttributeDefinition
+    {
+        public int Id { get; set; }
+        public string Definition { get; set; }
+        public AttributeDataType DataType {get; set;}
+        public bool IsPredefined { get; set; }
+        public bool AllowMultipleValues { get; set; }
+        public override bool Equals(object obj)
+        {
+            return Definition.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Definition.GetHashCode();
+        }
+    }
+
+    public enum AttributeDataType
+    {
+        String = 0
+    }
+
+    public class Attribute
+    {
+        public int Id { get; set; }
+        public int DefinitionId { get; set; }
+        public AttributeDefinition Definition { get; set; }
+        public string Value { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var attr = obj as Attribute;
+
+            return attr != null && Definition.Equals(attr.Definition) && Value.Equals(attr.Value);
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 23;
+            unchecked
+            {
+                hash = hash * 31 + Definition.GetHashCode();
+                hash = hash * 31 + Value.GetHashCode();
+            }
+            return hash;
+        }
+    }
+
+    public class NodeAttribute
+    {
+        public int Id { get; set; }
+        public int NodeId { get; set; }
+        public int Rank { get; set; }
+        public Attribute Attribute { get; set; }
+    }
+
+    public class Link
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// First Node in a Link
+        /// </summary>
+        public Node NodeA { get; set; }
+
+        /// <summary>
+        /// Second Node in a Link
+        /// </summary>
+        public Node NodeB { get; set; }
+
+        /// <summary>
+        /// Type of Link
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Strength of Link
+        /// </summary>
+        public float Strength { get; set; }
+
+        /// <summary>
+        /// Direction of Link
+        /// </summary>
+        public LinkFlow Direction { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var link = obj as Link;
+
+            return link != null && NodeA.Equals(link.NodeA) && NodeB.Equals(link.NodeB) && Type.Equals(link.Type);
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 23;
+            unchecked
+            {
+                hash = hash * 31 + NodeA.GetHashCode();
+                hash = hash * 31 + NodeB.GetHashCode();
+                hash = hash * 31 + Type.GetHashCode();
+            }
+            return hash;
+        }
+    }
+
+    /// <summary>
+    /// Represents the direction of the ILink. Default Bidirectional
+    /// </summary>
+    public enum LinkFlow
+    {
+        /// <summary>
+        /// Link flows between both nodes
+        /// </summary>
+        Bidirectional = 0,
+        /// <summary>
+        /// Link flows from Node A to Node B
+        /// </summary>
+        AtoB = 1,
+        /// <summary>
+        /// Link flows from Node B to Node A
+        /// </summary>
+        BtoA = 2
+    }
+}
---
+++ src/HDLink.Provider.EntityFramework/NodeDBContext.cs
@@ -0,0 +1,70 @@
+﻿using HDLink.Core;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Infrastructure;
+using Microsoft.Data.Entity.Metadata;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HDLink.Provider.EntityFramework
+{
+    public class NodeDbContext : DbContext
+    {
+        const string DefinitionId = "DefinitionId";
+
+        public DbSet<Link> Links { get; set; }
+        public DbSet<Node> Nodes { get; set; }
+        public DbSet<Attribute> Attributes { get; set; }
+        public DbSet<AttributeDefinition> AttributeDefinitions { get; set; }
+        public DbSet<NodeAttribute> NodeAttribute { get; set; }
+
+        public NodeDbContext() : base() { }
+        public NodeDbContext(DbContextOptions options) : base(options) { }
+        public NodeDbContext(System.IServiceProvider serviceProvider) : base(serviceProvider) { }
+        public NodeDbContext(System.IServiceProvider serviceProvider,DbContextOptions options) : base(serviceProvider,options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AttributeDefinition>().ToTable(nameof(AttributeDefinitions));
+            modelBuilder.Entity<AttributeDefinition>().HasKey(r => r.Id);
+            modelBuilder.Entity<AttributeDefinition>().Property(r => r.Definition).HasMaxLength(255);
+            modelBuilder.Entity<AttributeDefinition>().HasIndex(r => r.Definition).IsUnique();
+            modelBuilder.Entity<AttributeDefinition>().Property(r => r.Id).ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<Attribute>().ToTable(nameof(Attributes));
+            modelBuilder.Entity<Attribute>().HasKey(r => r.Id);
+            modelBuilder.Entity<Attribute>().Property(r => r.Value).HasMaxLength(255);
+            modelBuilder.Entity<Attribute>().Property(r => r.Id).ValueGeneratedOnAdd();
+            //modelBuilder.Entity<Attribute>().Property<int>(DefinitionId);
+
+            modelBuilder.Entity<Attribute>().HasOne(r => r.Definition).WithMany()
+                .HasForeignKey(DefinitionId)
+                .IsRequired();
+            modelBuilder.Entity<Attribute>().HasIndex(DefinitionId, nameof(Attribute.Value)).IsUnique();
+
+            
+            
+
+            modelBuilder.Entity<Node>().ToTable(nameof(Nodes));
+            modelBuilder.Entity<Node>().HasKey(r => r.Id);
+            modelBuilder.Entity<Node>().Property(r => r.Id).ValueGeneratedOnAdd();
+            modelBuilder.Entity<Node>().HasMany(r => r.Attributes).WithOne()
+                .IsRequired().OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Link>().ToTable(nameof(Links));
+            modelBuilder.Entity<Link>().HasKey(r => r.Id);
+            modelBuilder.Entity<Link>().Property(r => r.Id).ValueGeneratedOnAdd();
+            modelBuilder.Entity<Link>().HasOne(r => r.NodeA).WithOne()
+                .IsRequired(false);
+            modelBuilder.Entity<Link>().HasOne(r => r.NodeB).WithOne()
+                .IsRequired(false);
+
+            modelBuilder.Entity<NodeAttribute>().ToTable(nameof(NodeAttribute));
+            modelBuilder.Entity<NodeAttribute>().HasKey(r => r.Id);
+            modelBuilder.Entity<NodeAttribute>().Property(r => r.Id).ValueGeneratedOnAdd();
+            modelBuilder.Entity<NodeAttribute>().HasOne(r=> r.Attribute).WithOne()
+                .IsRequired().OnDelete(DeleteBehavior.Cascade);
+        }
+
+    }
+}
